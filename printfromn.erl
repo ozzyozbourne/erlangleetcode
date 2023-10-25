2beef359998bf05a5406f75b7e24ebdd09bc6fd4
ozzy ozbourne <khanosaid726@gmail.com>
2023-10-25 19:19:00 +0530

    one fix

--- printfromn.erl
+++ printfromn.erl
@@ -1,5 +1,13 @@
 -module(printfromn).
--export([print/1, sum_digits/2, in_sequence/1, is_palindrome/4]).
+-export([print/1, 
+         sum_digits/2, 
+         in_sequence/1, 
+         is_palindrome/4, 
+         maxi/1,
+        permute/1]).
+
+permute([])   -> [[]];
+permute(List) -> [ [H | T] || H <- List, T <- permute(List -- [H])].
 
 print(0)  -> ok;
 print(N)  -> io:format("~w~n", [N]),
@@ -9,11 +17,14 @@ sum_digits(0,   Sum) -> Sum;
 sum_digits(Num, Sum) -> sum_digits(Num div 10, Sum + Num rem 10).
                  
 in_sequence([_ | []]) -> true;
-in_sequence([H | T]) -> if H > hd(T) -> false;
+in_sequence([H | T])  -> if H > hd(T) -> false;
                            true -> in_sequence(T)
                                    end.
     
-is_palindrome(_, _, _, false) -> false;
+is_palindrome(_, _, _, false)         -> false;
 is_palindrome(_, L, R, _) when L >= R -> true;
-is_palindrome(S, L, R, _) -> is_palindrome(S, L +1 , R - 1, lists:nth(L, S) == lists:nth(R, S)).
+is_palindrome(S, L, R, _)             -> is_palindrome(S, L +1 , R - 1, lists:nth(L, S) == lists:nth(R, S)).
                                      
+
+maxi([H | []]) -> H;
+maxi([H | T])  -> max(H , maxi(T)).
